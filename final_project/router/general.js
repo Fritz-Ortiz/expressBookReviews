b8ef1f93bfd79818d3a656fd70541449f71c9dbc
Fritz Ortiz <fritzortiz.27@gmail.com>
2023-11-13 23:17:37 -0500

    added  the code for getting the list of books available in the shop,
    the code for getting the
    book details based on ISBN,
    
    the code for getting the
    book details based on the author,
    
    the code for getting the book
    details based on the title
    
    and the code for getting book reviews

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,31 +13,64 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn])
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const matchingBooks = [];
+
+  for (const key in books) {
+    if (books.hasOwnProperty(key)) {
+      const book = books[key];
+      if (book.author.toLowerCase() === author.toLowerCase()) {
+        matchingBooks.push(book);
+      }
+    }
+  }
+
+  res.json(matchingBooks);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const matchingBooks = [];
+  for (const key in books) {
+    if (books.hasOwnProperty(key)) {
+      const book= books[key];
+      if (book.title.toLowerCase() === title.toLowerCase()) {
+        matchingBooks.push(book);
+      }
+    }
+  }
+
+  res.json(matchingBooks);
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/review/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    const matchingBooks = [];
+  
+    if (books.hasOwnProperty(isbn)) {
+      const book = books[isbn];
+      if (book.hasOwnProperty("reviews")) {
+        matchingBooks.push(book.reviews);
+      }
+    }
+  
+    res.send(matchingBooks);
+  });
 
 module.exports.general = public_users;
